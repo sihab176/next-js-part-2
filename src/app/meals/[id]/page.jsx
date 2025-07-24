6a7bc4981da91ee6a07208337541517c65ddddef
unknown <shariar2739@gmail.com>
2025-07-25 00:33:50 +0600

    Image tag to next js

---
+++ src/app/meals/[id]/page.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+// handleSingle data =======>
+const handleSingleData = async (id) => {
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  const data = res.json();
+  return data;
+};
+// next js meta tag =========>
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const single = await handleSingleData(id);
+  return {
+    title: single.strCategory,
+    description: single.strInstructions,
+  };
+}
+// component =================>
+const MealsDetails = async ({ params }) => {
+  const id = await params.id;
+  const data = await handleSingleData(id);
+  const [mainData] = await data.meals;
+
+
+  return (
+    <div>
+      <div className="border m-4 flex justify-center items-center">
+        <div className=" flex-1">
+          <img className=" w-[400px]" src={`${mainData.strMealThumb}`} alt="" />
+        </div>
+        <div className="flex-1">
+          <p className="text-2xl text-green-500"> {mainData?.strMeal} </p>
+          <p className=" "> Area : {mainData?.strArea} </p>
+          <p className=" "> Category : {mainData?.strCategory} </p>
+          <p className=" text-gray-500"> Description : {mainData?.strInstructions} </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MealsDetails;
--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-
-export const singlePost = async (post_id) => {
+// ! handle single Post by id ====>
+export const getSinglePost = async (post_id) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${post_id}`
   );
   const data = res.json();
   return data;
 };
-
+//! next js meta tag =============>
+export async function generateMetadata({ params }) {
+  // read route params
+  const { id } = await params;
+  // fetch data
+  const singlePost = await getSinglePost(id);
+  return {
+    title: singlePost.title,
+    description: singlePost.body,
+  };
+}
+// =======================  components ==========================>
 const postDetails = async ({ params }) => {
   const id = await params.id;
-  const singleData = await singlePost(id);
+  const singleData = await getSinglePost(id);
   console.log(singleData);
 
   return (
